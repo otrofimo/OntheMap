5ef2971f8fc753841379698086faaf0013cdfaaf
Oleg Trofimov <otrofimo@gmail.com>
2015-10-04 13:33:05 -0700

    visual set for login controller

---
+++ OntheMap/StudentInformation.swift
@@ -0,0 +1,15 @@
+//
+//  StudentInformation.swift
+//  OntheMap
+//
+//  Created by Oleg Trofimov on 10/4/15.
+//  Copyright © 2015 Oleg Trofimov. All rights reserved.
+//
+
+import Foundation
+
+struct StudentInformation {
+    init(locations: [String:AnyObject]) {
+        
+    }
+}
